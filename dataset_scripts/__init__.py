f8e5afb2a98e989ccd22dd1d41eb7ff3e7573e3e
woooodyy <1018940210@qq.com>
2023-07-26 19:38:20 +0800

    feat: init

---
+++ dataset_scripts/__init__.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+"""
+@File     : __init__.py.py
+@Project  : Robust_Data
+@Time     : 2022/10/1 14:52
+@Author   : Zhiheng Xi
+"""
+
+if __name__ == '__main__':
+    pass
